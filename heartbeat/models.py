d889f20e538caf02ef6b58ce0ec60542db3f2ef6
Jiaji Wang <jiaji.wang@gmail.com>
2013-04-12 15:39:50 +0800

    bug fix

--- heartbeat/models.py
+++ heartbeat/models.py
@@ -19,13 +19,13 @@ class BaseModel(models.Model):
         return ':'.join((self.__class__.__name__, str(self.pk), field))
 
     def get_redis_set(self, field):
-        key = self.get_redis_key(field):
+        key = self.get_redis_key(field)
         return self.__class__.redis_server.smembers(key)
 
     @task()
     def add_redis_set(self, field, value):
-        key = self.get_redis_key(field):
-        self.__class__.redis_server.sadd(value):
+        key = self.get_redis_key(field)
+        self.__class__.redis_server.sadd(value)
         
     @task()
     def del_redis_key(self, field):
