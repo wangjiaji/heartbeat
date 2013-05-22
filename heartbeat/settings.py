6399bfce452ed013b9b0c86d7ba3411c65854186
Jiaji Wang <jiaji.wang@gmail.com>
2013-05-22 15:46:21 +0800

    Added admin static files

--- heartbeat/settings.py
+++ heartbeat/settings.py
@@ -57,7 +57,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = 'https://hbbeatimages.s3.amazonaws.com/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -67,13 +67,14 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = 'https://hbbeatimages.s3.amazonaws.com/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/usr/local/lib/python2.7/dist-packages/django/contrib/admin/static/',
 )
 
 # List of finder classes that know how to find static files in
